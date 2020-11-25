a386f6b7b9ebe8a51cec76262acb0fe2dbf5a2b0
landaverde1196 <alejndro1196@gmail.com>
2020-11-24 21:02:12 -0600

    Users page body

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h1>Users Works!</h1>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Alejandro",
+      image:
+        "https://www.treebo.com/blog/wp-content/uploads/2017/12/baga_beach__north_go_QvVbp.jpg",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
