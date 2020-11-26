456d4e573602c524bfe829d402d844b7110cf8c4
landaverde1196 <alejndro1196@gmail.com>
2020-11-25 19:05:30 -0600

    Avatar.js and Avatar.css added

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./UserItem.css";
 
+import Avatar from "../../shared/components/Avatar";
+
 const UserItem = (props) => {
   return (
     <li className="user-item">
       <div className="user-item__content">
         <div className="user-item__image">
-          <img src={props.image} alt={props.name} />
+          <Avatar image={props.image} alt={props.name} />
         </div>
         <div className="user-item__info">
           <h2>{props.name}</h2>
