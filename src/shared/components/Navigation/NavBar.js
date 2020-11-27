dcc2d6aac3ea46fad976723f8c1e2c9f866bda04
landaverde1196 <alejndro1196@gmail.com>
2020-11-26 18:57:08 -0600

    NavLinks added with Css

--- src/shared/components/Navigation/NavBar.js
+++ src/shared/components/Navigation/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { NavLink } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
@@ -11,16 +12,17 @@ const NavBar = () => {
       <Navbar.Brand href="/">
         <h1>Spots</h1>
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" className="toggle" />
+      <Navbar.Collapse id="responsive-navbar-nav" collapseOnSelect>
         <Nav className="mr-auto"></Nav>
-        <Nav>
-          <Nav.Link href="/places">
-            <h6>Places</h6>
-          </Nav.Link>
-          <Nav.Link href="/users">
-            <h6>Users</h6>
-          </Nav.Link>
+
+        <Nav className="nav-links">
+          <NavLink to="/" exact>
+            ALL USERS
+          </NavLink>
+          <NavLink to="/u1/places">MY PLACES</NavLink>
+          <NavLink to="/places/new">ADD PLACE</NavLink>
+          <NavLink to="/auth">AUTHENTICATE</NavLink>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
